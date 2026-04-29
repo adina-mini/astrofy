95193faf70a91adf5fbfa60cfd3c4b28e04dc355
adina-mini <adinarehman018@gmail.com>
2026-04-29 17:53:30 +0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Adina Rehman | AI Automation Engineer';
+export const SITE_DESCRIPTION = 'Specializing in RAG workflows, n8n automation, and scalable AI infrastructure.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
